dbc03616cf821d5155ddb282f4adde422f1854bb
stpaynter <simeonpaynter@gmail.com>
2022-07-17 21:00:06 -0500

    updated app with streaming hostory graphs and summary statistics

--- server.R
+++ server.R
@@ -5,52 +5,105 @@ library(tidyverse)
 
 server <- function(input, output){
   
-  # Streaming History Data
+  # Tab 2: Streaming History Data
   streamingHistory <- reactive({
-    files <- req(input$streamingHistory)
-    
-    stream <- c()
-    
-    for(i in 1:length(files[, 1])){
-      temp_df <- jsonlite::fromJSON(
-        files[[i, 'datapath']]
-      )
-      stream <- rbind(stream, temp_df)
-    }
-    
-    # transform data
-    history <- stream
-    history$endTime_ymd <- as.Date(history$endTime)
-    history$endTime_ymd_hm <- lubridate::ymd_hm(history$endTime)
-    history$wday = lubridate::wday(history$endTime_ymd, label = TRUE)
-    
-    return(history)
+      files <- req(input$streamingHistory)
+      
+      stream <- c()
+      
+      for(i in 1:length(files[, 1])){
+        temp_df <- jsonlite::fromJSON(
+          files[[i, 'datapath']]
+        )
+        stream <- rbind(stream, temp_df)
+      }
+      
+      # transform data
+      history <- stream
+      history$endTime_ymd <- as.Date(history$endTime)
+      history$endTime_ymd_hm <- lubridate::ymd_hm(history$endTime)
+      history$wday = lubridate::wday(history$endTime_ymd, label = TRUE)
+      
+      return(history)
   })
   
-  # Streaming History
   output$DateRange <- renderUI({
-    minDate <- min(streamingHistory()$endTime_ymd)
-    maxDate <- max(streamingHistory()$endTime_ymd)
-    
-    dateRangeInput("dateRange", "Date Listened To:", start = minDate, end = maxDate, format = "mm/dd/yyyy")
+      minDate <- min(streamingHistory()$endTime_ymd)
+      maxDate <- max(streamingHistory()$endTime_ymd)
+      
+      dateRangeInput("dateRange", "Date Listened To:", start = minDate, end = maxDate, format = "mm/dd/yyyy")
+  })
+  
+  history_filtered <- reactive({
+      req(input$dateRange)
+      streamingHistory() %>%
+        filter(endTime >= input$dateRange[1] & endTime <= input$dateRange[2]) %>%
+        return()
+  })
+  
+  output$TotalListeningTime <- renderText({
+      total_time <- history_filtered() %>%
+        summarise(mins = round(sum(msPlayed)/3600000))
+      
+      return(paste("Total Listening Time:", sum(total_time$mins), "Hours"))
+  })
+  
+  output$AvgListeningTime <- renderText({
+      avg_time <- history_filtered() %>%
+        group_by(endTime_ymd) %>%
+        summarise(mins = round(sum(msPlayed)/60000)) %>%
+        ungroup()
+      
+      return(paste("Avg. Daily Listening Time:", round(mean(avg_time$mins)), "Minutes"))
+  })
+  
+  output$NumSongs <- renderText({
+      n_songs <- history_filtered() %>%
+        unique() %>%
+        nrow()
+      return(paste("# of Unique Songs Played:", n_songs))
+  })
+  
+  top_artists <- reactive({
+    history_filtered() %>%
+      group_by(artistName) %>%
+      summarise(n = n(), min = round(sum(msPlayed)/60000)) %>%
+      ungroup() %>%
+      arrange(desc(min)) %>%
+      head(10)
+  })
+  
+  output$TopArtists <- renderPlot({
+    top_artists() %>%  
+      ggplot(aes(x = reorder(artistName, +min), y = min)) + 
+      geom_col(fill="#1ed760") +
+      coord_flip() +
+      labs(title = "Top Artists",
+           y= "Minutes Played",
+           x = "Song") +
+      theme_classic() +
+      scale_y_continuous(expand = c(0, 0), limits = c(0, NA), breaks = seq.int(0, 10000, 60))
   })
   
   top_songs <- reactive({
-    streamingHistory() %>%
-    group_by(trackName, artistName) %>%
-    summarise(n = n()) %>%
-    filter(n > 50) %>%
-    arrange(desc(n))
+      history_filtered() %>%
+      group_by(trackName, artistName) %>%
+      summarise(n = n(), min = round(sum(msPlayed)/60000)) %>%
+      ungroup() %>%
+      arrange(desc(min)) %>%
+      head(10)
   })
   
-  output$StreamPlot1 <- renderPlot({
-    top_songs() %>%  
-      ggplot(aes(x = reorder(trackName, +n), y = n)) + 
-      geom_col(fill="#1ed760") +
-      coord_flip() +
-      labs(y= "# of Plays",
-           x = "Song") +
-      theme_classic() +
-      scale_y_continuous(expand = c(0, 0), limits = c(0, NA), breaks = seq.int(0, 100, 10))
+  output$TopSongs <- renderPlot({
+      top_songs() %>%  
+        ggplot(aes(x = reorder(trackName, +min), y = min)) + 
+        geom_col(fill="#1ed760") +
+        coord_flip() +
+        labs(title = "Top Songs",
+             y= "Minutes Played",
+             x = "Song") +
+        theme_classic() +
+        scale_y_continuous(expand = c(0, 0), limits = c(0, NA), breaks = seq.int(0, 10000, 60))
   })
 }
+
--- ui.R
+++ ui.R
@@ -6,18 +6,34 @@ library(tidyverse)
 ui <- fluidPage(
   titlePanel(title = "Spotify Listening Activity", windowTitle = "Spotify Data Analysis"),
   tabsetPanel(type = "tabs",
+              
+              # Tab 1:
               tabPanel("File Upload", 
-                       fileInput("streamingHistory", "Upload 'StreamingHistory.json' files:", 
+                       br(),
+                       h5("After downloading your Spotify data, you will have access to the following files. Please upload them."),
+                       br(),
+                       fileInput("streamingHistory", "'StreamingHistory.json' file(s):", 
                                  multiple = TRUE, accept = c(".json")),
                        
-                       fileInput("yourLibrary", "Upload 'YourLibrary.json' file",
+                       fileInput("yourLibrary", "'YourLibrary.json' file:",
                                  multiple = FALSE, accept = c(".json"))
                        ),
+              
+              # Tab 2:
               tabPanel("Streaming History",
                        uiOutput("DateRange"),
-                       plotOutput("StreamPlot1")),
+                       textOutput("TotalListeningTime"),
+                       textOutput("AvgListeningTime"),
+                       textOutput("NumSongs"),
+                       plotOutput("TopArtists"),
+                       plotOutput("TopSongs")),
+              
+              # Tab 3:
               tabPanel("Your Library"),
-              tabPanel("Read Me Page",
-                       h6())
+              
+              # Tab 4:
+              tabPanel("Read Me",
+                       h5("Request 1 year of Spotfiy data by following the steps on this page:"), 
+                       a("Spotify - Data rights and privacy settings", href = "https://support.spotify.com/us/article/data-rights-and-privacy-settings/"))
               )
   )
